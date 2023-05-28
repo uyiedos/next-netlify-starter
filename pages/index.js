3b2740622b835997f603d187c17c2ec08199a670
Uyi Edomwandagbon <32402168+uyiedos@users.noreply.github.com>
2023-05-28 12:23:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-        <iframe
+    <iframe
     src="https://ipfs-2.thirdwebcdn.com/ipfs/QmSmkcH3AzLPTcub173t7bCtTzQV3dRb4gwG3uU84J6YJq?contract=0xfF08C4885dd2644A4e24CD6DFA983bf2F4982E49&chain=%7B%22name%22%3A%22PulseChain+Testnet+v4%22%2C%22chain%22%3A%22t4PLS%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fpulsechain-testnet-v4.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Test+Pulse%22%2C%22symbol%22%3A%22tPLS%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22t4pls%22%2C%22chainId%22%3A943%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22pulsechain-testnet-v4%22%7D&primaryColor=purple"
     width="601px"
     height="601px"
     style="max-width:100%;"
     frameborder="0"
-    ></iframe>
+    >
+    </iframe>
      
